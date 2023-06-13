9be20ea6923d009b0ff96888ca84875387f1ff53
Yout <youtlone95@gmail.com>
2023-06-13 18:19:05 +0630

    jest testing tasks

---
+++ length.js
@@ -0,0 +1,8 @@
+function stringLength(string){
+    const length = string.length;
+  if (length < 1 || length > 10) {
+    throw new Error('Invalid string length!');
+  }
+  return length;
+}
+module.exports = stringLength;
---
+++ reverse.test.js
@@ -0,0 +1,8 @@
+const reverseString = require('./reverse');
+describe('reverseString', () => {
+    test('returns the reversed string', () => {
+      expect(reverseString('hello')).toBe('olleh');
+      expect(reverseString('abcd1234')).toBe('4321dcba');
+    });
+  });
+  
